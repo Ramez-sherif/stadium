31bd2ebf78a3a71db54eaeb050f1e3e0f64e3c58
Ramez-sherif <ramessherif@gmail.com>
2023-05-12 20:08:09 +0300

    Updated stadium category model

--- src/main/java/com/example/softwareproject/stadium/models/StadiumCategories.java
+++ src/main/java/com/example/softwareproject/stadium/models/StadiumCategories.java
@@ -20,4 +20,29 @@ public class StadiumCategories {
     @ManyToOne
     @JoinColumn(name = "category_id")
     Category category;
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Stadium getStadium() {
+        return this.stadium;
+    }
+
+    public void setStadium(Stadium stadium) {
+        this.stadium = stadium;
+    }
+
+    public Category getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
