5c70de35bc9f1e544fd6509e39bd1995a82fc304
rdahshan <ranaadahshan@gmail.com>
2023-05-12 15:17:27 +0300

    AddStd,AddCat-FE

--- src/main/java/com/example/softwareproject/stadium/controllers/StadiumController.java
+++ src/main/java/com/example/softwareproject/stadium/controllers/StadiumController.java
@@ -1,10 +1,11 @@
 package com.example.softwareproject.stadium.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.softwareproject.stadium.services.StadiumService;
-
+//@Controller
 @RestController
 public class StadiumController {
     @Autowired
