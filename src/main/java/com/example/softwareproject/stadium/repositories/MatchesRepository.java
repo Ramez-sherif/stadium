8d97a3d0fb59fef0b36c741a6dc31739960b6d10
Ramez-sherif <ramessherif@gmail.com>
2023-05-12 03:40:14 +0300

    Updated MatchRepsitory,& Add StadiumCategoriesRepository

--- src/main/java/com/example/softwareproject/stadium/repositories/MatchesRepository.java
+++ src/main/java/com/example/softwareproject/stadium/repositories/MatchesRepository.java
@@ -1,11 +1,16 @@
 package com.example.softwareproject.stadium.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.softwareproject.stadium.models.Matches;
+import com.example.softwareproject.stadium.models.Teams;
+import com.example.softwareproject.stadium.models.Tournaments;
 
 @Repository
 public interface MatchesRepository extends JpaRepository<Matches,Long> {
-    
+    List<Matches>findByTeam1OrTeam2(Teams team1,Teams team2);
+    List<Matches>findByTournment(Tournaments tournaments);
 }
